377133b12a99951dfe8a9f46481657e1b774336b
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-01-25 22:35:16 -0500

    first commit after amend actions

---
+++ frontend/src/containers/CompanyContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class CompanyContainer extends Component {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <h1>Company LookUp</h1>
+        );
+    };
+
+}
